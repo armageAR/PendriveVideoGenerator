3cb3b35d4052e81510b6fe7738c2fa2c899a1c07
armage <armage@gmail.com>
2019-05-31 11:50:18 -0300

    list all videos  view

--- app/videos/views.py
+++ app/videos/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Video
 
 
-def index(request):
-    return render(request, "videos/index.html", {})
+def video_list_view(request):
+    queryset = Video.objects.all()  # list of objects
+    context = {
+        "object_list": queryset
+    }
+    return render(request, "videos/list.html", context)
